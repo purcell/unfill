2547870e325c8f78a666e7f646d70c4214a8cd66
Steve Purcell <steve@sanityinc.com>
2012-05-29 12:50:30 +0100

    Minor docstring fixes

--- unfill.el
+++ unfill.el
@@ -13,15 +13,15 @@
 ;;;###autoload
 (defun unfill-paragraph ()
   "Replace newline chars in current paragraph by single spaces.
-This command does the reverse of `fill-paragraph'."
+This command does the inverse of `fill-paragraph'."
   (interactive)
   (let ((fill-column most-positive-fixnum))
     (fill-paragraph nil)))
 
 ;;;###autoload
 (defun unfill-region (start end)
-  "Replace newline chars in region by single spaces.
-This command does the reverse of `fill-region'."
+  "Replace newline chars in region from START to END by single spaces.
+This command does the inverse of `fill-region'."
   (interactive "r")
   (let ((fill-column most-positive-fixnum))
     (fill-region start end)))
