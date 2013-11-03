39e2bb0bfc62c77fc2889ef8e1acde43e0e81a04
Steve Purcell <steve@sanityinc.com>
2013-11-03 10:13:45 +0000

    Prefer use-region-p to region-active-p

--- unfill.el
+++ unfill.el
@@ -26,5 +26,32 @@ This command does the inverse of `fill-region'."
   (let ((fill-column most-positive-fixnum))
     (fill-region start end)))
 
+;;;###autoload
+(defun toggle-fill-unfill ()
+  "Remove or add line ending chars on current paragraph.  This command is similar to a toggle of `fill-paragraph'.  When there is a text selection, act on the region."
+  (interactive)
+  ;; This command symbol has a property “'stateIsCompact-p”.
+  (let (currentStateIsCompact
+        (bigFillColumnVal most-positive-fixnum)
+        (deactivate-mark nil))
+    (save-excursion
+      ;; Determine whether the text is currently compact.
+      (setq currentStateIsCompact
+            (if (eq last-command this-command)
+                (get this-command 'stateIsCompact-p)
+              (if (> (- (line-end-position) (line-beginning-position)) fill-column) t nil) ) )
+
+      (if (use-region-p)
+          (if currentStateIsCompact
+              (fill-region (region-beginning) (region-end))
+            (let ((fill-column bigFillColumnVal))
+              (fill-region (region-beginning) (region-end))) )
+        (if currentStateIsCompact
+            (fill-paragraph nil)
+          (let ((fill-column bigFillColumnVal))
+            (fill-paragraph nil)) ) )
+
+      (put this-command 'stateIsCompact-p (if currentStateIsCompact nil t)) ) ) )
+
 (provide 'unfill)
 ;;; unfill.el ends here
